74bcd52ff60ceb809c25a27bfb1088d3def3d474
Benito Go III <benitogothethird@hotmail.com>
2024-11-11 02:25:32 +0800

    Used timer instead of separate thread for mu helper

--- Source Main 5.2/source/Winmain.cpp
+++ Source Main 5.2/source/Winmain.cpp
@@ -8,6 +8,7 @@
 #include <dpapi.h>
 #include <locale.h>
 #include <zmouse.h>
+#include <cmath>
 #include "UIWindows.h"
 #include "UIManager.h"
 #include "ZzzOpenglUtil.h"
@@ -36,6 +37,7 @@
 #include "./Utilities/Log/muConsoleDebug.h"
 #include "ProtocolSend.h"
 #include "ProtectSysKey.h"
+#include "MUHelper/MuHelper.h"
 
 #include "CBTMessageBox.h"
 #include "./ExternalObject/leaf/regkey.h"
@@ -1450,20 +1452,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLin
         g_ErrorReport.Write(L".net runtime failed to load :(L");
     }
 
-    switch (WindowHeight)
-    {
-        case 480:FontHeight = 13; break;
-        case 600:FontHeight = 14; break;
-        case 768:FontHeight = 16; break;
-        case 900:FontHeight = 16; break;
-        case 1024:FontHeight = 16; break;
-        case 1050:FontHeight = 17; break;
-        case 1080: FontHeight = 17; break;
-        case 1200: FontHeight = 18; break;
-        case 1280: FontHeight = 18; break;
-        case 1400: FontHeight = 19; break;
-        case 1440: FontHeight = 20; break;
-    }
+    FontHeight = static_cast<int>(std::ceil(12 + ((WindowHeight - 480)/200.f)));
 
     int nFixFontHeight = WindowHeight <= 600 ? 14 : 15;
     int nFixFontSize;
@@ -1508,6 +1497,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLin
     }
 
     SetTimer(g_hWnd, HACK_TIMER, 20 * 1000, NULL);
+    SetTimer(g_hWnd, MUHELPER_TIMER, 200 /* ms */, CMuHelper::TimerProc);
 
     srand((unsigned)time(NULL));
     for (int i = 0; i < 100; i++)
